ff09d0eeacc651410337df9ea228c13690d2db75
TonyAdo <coolhzb@163.com>
2014-12-15 19:41:53 +0800

    add a SortedListPriorityQueue....

--- Lab06/src/com/xqbase/java/SortedListPriorityQueue.java
+++ Lab06/src/com/xqbase/java/SortedListPriorityQueue.java
@@ -1,20 +1,40 @@
 package com.xqbase.java;
 
+import com.xqbase.java.list.NodePositionList;
+import com.xqbase.java.list.PositionList;
+
+import java.util.Comparator;
+
 public class SortedListPriorityQueue<K, V> implements PriorityQueue<K, V>{
+
+    private PositionList<Entry<K, V>> entries;
+    private Comparator<K> c;
+
+    public SortedListPriorityQueue() {
+        entries = new NodePositionList<Entry<K, V>>();
+        c = new DefaultComparator<K>();
+    }
+
+    public SortedListPriorityQueue(Comparator<K> comparator) {
+        entries = new NodePositionList<Entry<K, V>>();
+        c = comparator;
+    }
 
     @Override
     public int size() {
-        return 0;
+        return entries.size();
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return entries.isEmpty();
     }
 
     @Override
     public Entry<K, V> min() {
-        return null;
+        if (isEmpty())
+            return null;
+        return entries.first().element();
     }
 
     @Override
@@ -25,5 +45,26 @@ public class SortedListPriorityQueue<K, V> implements PriorityQueue<K, V>{
     @Override
     public Entry<K, V> removeMin() {
         return null;
+    }
+
+    protected static class MyEntry<K, V> implements Entry<K, V> {
+
+        protected K key;
+        protected V value;
+
+        public MyEntry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public K getKey() {
+            return key;
+        }
+
+        @Override
+        public V getValue() {
+            return value;
+        }
     }
 }
