ef158aa70a1f34b467c65bafee246c235141b5d5
TonyAdo <coolhzb@163.com>
2014-12-15 19:33:30 +0800

    add the NodePositionList implementation...

--- Lab06/src/com/xqbase/java/list/NodePositionList.java
+++ Lab06/src/com/xqbase/java/list/NodePositionList.java
@@ -18,63 +18,109 @@ public class NodePositionList<E> implements PositionList<E> {
         head.setNext(tail);
     }
 
+    // TODO: Update the check logical
+    private DNode<E> checkPosition(Position<E> p) {
+        DNode<E> temp = (DNode<E>) p;
+        return temp;
+    }
+
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return (size == 0);
     }
 
     @Override
     public Position<E> first() {
-        return null;
+        if (isEmpty())
+            return null;
+
+        return head.getNext();
     }
 
     @Override
     public Position<E> last() {
-        return null;
+        if (isEmpty())
+            return null;
+
+        return tail.getPrev();
     }
 
     @Override
     public Position<E> next(Position<E> p) {
-        return null;
+        DNode<E> v = checkPosition(p);
+        DNode<E> next = v.getNext();
+        if (next  == tail)
+            return null;
+        return next;
     }
 
     @Override
     public Position<E> prev(Position<E> p) {
-        return null;
+        DNode<E> v = checkPosition(p);
+        DNode<E> prev = v.getPrev();
+        if (prev == head)
+            return null;
+        return prev;
     }
 
     @Override
     public void addFirst(E element) {
-
+        size++;
+        DNode<E> newNode = new DNode<E>(head, head.getNext(), element);
+        head.getNext().setPrev(newNode);
+        head.setNext(newNode);
     }
 
     @Override
     public void addLast(E element) {
-
+        size++;
+        DNode<E> newNode = new DNode<E>(tail.getPrev(), tail, element);
+        tail.getPrev().setNext(newNode);
+        tail.setPrev(newNode);
     }
 
     @Override
     public void addAfter(Position<E> p, E element) {
-
+        DNode<E> v = checkPosition(p);
+        size++;
+        DNode<E> newNode = new DNode<E>(v, v.getNext(), element);
+        v.getNext().setPrev(newNode);
+        v.setNext(newNode);
     }
 
     @Override
     public void addBefore(Position<E> p, E element) {
-
+        DNode<E> v = checkPosition(p);
+        size++;
+        DNode<E> newNode = new DNode<E>(v.getPrev(), v, element);
+        v.getPrev().setNext(newNode);
+        v.setPrev(newNode);
     }
 
     @Override
     public E remove(Position<E> p) {
-        return null;
+        DNode<E> v = checkPosition(p);
+        size--;
+        DNode<E> prev = v.getPrev();
+        DNode<E> next = v.getNext();
+        prev.setNext(next);
+        next.setPrev(prev);
+        E vEle = v.getElement();
+        v.setPrev(null);
+        v.setNext(null);
+        return vEle;
     }
 
     @Override
     public E set(Position<E> p, E element) {
-        return null;
+        DNode<E> v = checkPosition(p);
+        E oldEle = v.getElement();
+        v.setElement(element);
+        return oldEle;
     }
 }
