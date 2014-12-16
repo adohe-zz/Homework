df41488448ce13083c6588b61161dcd208774813
TonyAdo <coolhzb@163.com>
2014-12-16 14:24:09 +0800

    finish the sorted list base priority queue implementation....

--- Lab06/src/com/xqbase/java/SortedListPriorityQueue.java
+++ Lab06/src/com/xqbase/java/SortedListPriorityQueue.java
@@ -1,6 +1,7 @@
 package com.xqbase.java;
 
 import com.xqbase.java.list.NodePositionList;
+import com.xqbase.java.list.Position;
 import com.xqbase.java.list.PositionList;
 
 import java.util.Comparator;
@@ -39,12 +40,39 @@ public class SortedListPriorityQueue<K, V> implements PriorityQueue<K, V>{
 
     @Override
     public Entry<K, V> insert(K key, V value) {
-        return null;
+        checkKey(key);
+        Entry<K, V> entry = new MyEntry<K, V>(key, value);
+        insertEntry(entry);
+        return entry;
+    }
+
+    /**
+     * Inserts the new entry into the sorted list
+     * and this takes O(n) time
+     */
+    private void insertEntry(Entry<K, V> entry) {
+        if (entries.isEmpty()) {
+            entries.addFirst(entry);
+        } else if (c.compare(entry.getKey(), entries.last().element().getKey()) > 0) {
+            entries.addLast(entry);
+        } else {
+            Position<Entry<K, V>> curr = entries.first();
+            while (c.compare(entry.getKey(), curr.element().getKey()) > 0) {
+                curr = entries.next(curr);
+            }
+            entries.addBefore(curr, entry);
+        }
+    }
+
+    private void checkKey(K key) {
+        // default do nothing
     }
 
     @Override
     public Entry<K, V> removeMin() {
-        return null;
+        if (entries.isEmpty())
+            return null;
+        return entries.remove(entries.first());
     }
 
     protected static class MyEntry<K, V> implements Entry<K, V> {
@@ -65,6 +93,17 @@ public class SortedListPriorityQueue<K, V> implements PriorityQueue<K, V>{
         @Override
         public V getValue() {
             return value;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 4, 2, 8, 0, 1, 5, 9};
+        SortedListPriorityQueue<Integer, Integer> queue = new SortedListPriorityQueue<Integer, Integer>();
+        for (int anArr : arr) {
+            queue.insert(anArr, anArr);
+        }
+        while (!queue.isEmpty()) {
+            System.out.println(queue.removeMin().getValue());
         }
     }
 }
