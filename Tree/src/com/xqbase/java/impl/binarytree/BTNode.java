183363d6fbb6c1a27f3a2b9b49b1e27b28c351bd
Tony <coolhzb@163.com>
2015-01-11 14:58:46 +0800

    finish the BTNode...

--- Tree/src/com/xqbase/java/impl/binarytree/BTNode.java
+++ Tree/src/com/xqbase/java/impl/binarytree/BTNode.java
@@ -7,43 +7,56 @@ package com.xqbase.java.impl.binarytree;
  */
 public class BTNode<E> implements BTPosition<E> {
 
+    // Element stored at this node
+    private E element;
+
+    private BTPosition<E> left, right, parent; // adjacent nodes.
+
+    // Constructor
+    public BTNode(E element, BTPosition<E> left, BTPosition<E> right, BTPosition<E> parent) {
+        setElement(element);
+        setLeft(left);
+        setRight(right);
+        setParent(parent);
+    }
+
     @Override
     public void setElement(E e) {
-
+        this.element = e;
     }
 
     @Override
     public BTPosition<E> getLeft() {
-        return null;
+        return this.left;
     }
 
     @Override
     public void setLeft(BTPosition<E> leftChild) {
-
+        this.left = leftChild;
     }
 
     @Override
     public BTPosition<E> getRight() {
-        return null;
+        return this.right;
     }
 
     @Override
     public void setRight(BTPosition<E> rightChild) {
-
+        this.right = rightChild;
     }
 
     @Override
     public BTPosition<E> getParent() {
-        return null;
+        return this.parent;
     }
 
     @Override
     public void setParent(BTPosition<E> parent) {
-
+        this.parent = parent;
     }
 
     @Override
     public E element() {
-        return null;
+        return this.element;
     }
 }
